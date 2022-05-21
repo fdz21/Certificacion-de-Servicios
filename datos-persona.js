bf2a08c3ee86f6e486b3c4523a9251ed58146148
FDZ <francodzalazar@gmail.com>
2022-05-20 21:30:14 -0300

    lic realizada

--- datos-persona.js
+++ datos-persona.js
@@ -1,13 +1,25 @@
-function datosPersonales(){
+function licenciaGoce(licencia){
+      if (licencia == ""){
+          licencia = ".-";
+          console.log(licencia)
+      }
+      return licencia
+  }
+  function datosPersonales(){
     let personaApellido = document.querySelector(".personaApellido");
     let personaNombre = document.querySelector(".personaNombre");
     let personaDni = document.querySelector(".dni");
+    let licencia = document.querySelector(".licencia");
     let form = document.querySelector("form");
     let apellidoInput = form.apellido.value;
     let nombreInput = form.nombre.value;
     let dniInput = form.dni.value;
+    let licenciaInput = form.licencia.value;
   
     personaApellido.innerHTML = apellidoInput;
     personaNombre.innerHTML = nombreInput;
     personaDni.innerHTML = dniInput;
+    licencia.innerHTML = licenciaGoce(licenciaInput);
   }
+
+  
